6357b19fd7b5dbaa7e3549b526937238051de93c
Rada Florin Daniel <florindaniel.rada@gameloft.com>
2023-04-19 19:03:24 +0300

    Added basic code for Offers api

--- models/offers/offers.go
+++ models/offers/offers.go
@@ -2,6 +2,7 @@ package offers
 
 import (
 	"back/database"
+	"errors"
 	"fmt"
 	"time"
 
@@ -20,22 +21,78 @@ type Offer struct {
 	BasePrice   int       `json:"base_price" gorm:"base_price"` // price is in cents to avoid rounding errors (99 -> 99 cents 0.99 Dollars)
 	Currency    string    `json:"currency" gorm:"currency"`
 	Description string    `json:"description" gorm:"description"`
-	MaxSyncRate time.Time `json:"max_sync_rate" gorm:"max_sync_rate"`
+	MaxSyncRate time.Time `json:"max_sync_rate" gorm:"max_sync_rate"` // how often can the system check the status
 	Published   bool      `json:"published" gorm:"published"`
 }
 
-func AddOffer(offer Offer) (Offer, error) {
-	err := database.PublicDB.Save(&offer)
-	if err.Error != nil {
-		return Offer{}, err.Error
+func AddOffer(offer Offer) error {
+	// Validate the offer fields
+	if offer.Name == "" {
+		return errors.New("Offer name cannot be empty")
+	}
+	if offer.MaxRunners < 0 {
+		return errors.New("MaxRunners cannot be negative")
+	}
+	if offer.BasePrice < 0 {
+		return errors.New("BasePrice cannot be negative")
+	}
+	if offer.Currency == "" {
+		return errors.New("Currency cannot be empty")
+	}
+	if offer.MaxSyncRate.IsZero() {
+		return errors.New("MaxSyncRate cannot be zero")
 	}
 
-	return offer, nil
+	// Add the offer to the database
+	result := database.PublicDB.Create(&offer)
+	if result.Error != nil {
+		return result.Error
+	}
+
+	return nil
 }
 
-func EditOffer(offer Offer) (Offer, error) {
+func EditOffer(id uint, updatedOffer Offer) error {
+	// Find the offer to update
+	var offer Offer
+	result := database.PublicDB.First(&offer, id)
+	if result.Error != nil {
+		return result.Error
+	}
 
-	return offer, nil
+	// Update the offer fields
+	offer.Name = updatedOffer.Name
+	offer.MaxRunners = updatedOffer.MaxRunners
+	offer.BasePrice = updatedOffer.BasePrice
+	offer.Currency = updatedOffer.Currency
+	offer.Description = updatedOffer.Description
+	offer.MaxSyncRate = updatedOffer.MaxSyncRate
+	offer.Published = updatedOffer.Published
+
+	// Save the changes to the database
+	result = database.PublicDB.Save(&offer)
+	if result.Error != nil {
+		return result.Error
+	}
+
+	return nil
+}
+
+func DeleteOffer(id uint) error {
+	// Find the offer to delete
+	var offer Offer
+	result := database.PublicDB.First(&offer, id)
+	if result.Error != nil {
+		return result.Error
+	}
+
+	// Delete the offer from the database
+	result = database.PublicDB.Delete(&offer)
+	if result.Error != nil {
+		return result.Error
+	}
+
+	return nil
 }
 
 func init() {
